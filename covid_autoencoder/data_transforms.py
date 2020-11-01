09e2ebeebe709eaf3be0cc97e5e9c59db44f7012
Andrew Marmon <drewmarmon@gmail.com>
2020-11-01 12:44:47 -0500

    created visualizable outputs

--- covid_autoencoder/data_transforms.py
+++ covid_autoencoder/data_transforms.py
@@ -9,21 +9,27 @@ def get_test_transforms(input_size,
                         pixel_std):
 
   return transforms.Compose([
-      transforms.Grayscale(),
       transforms.Resize(input_size),
+      transforms.Grayscale(),
       transforms.ToTensor(),
       transforms.Normalize(mean=pixel_mean, std=pixel_std)
   ])
 
+def get_no_transforms():
+
+  return transforms.Compose([
+      transforms.Grayscale(),
+      transforms.ToTensor()
+  ])
 
 def get_train_transforms(input_size,
                          pixel_mean,
                          pixel_std):
   return transforms.Compose([
-      transforms.Grayscale(),
       transforms.Resize(input_size),
-      transforms.RandomHorizontalFlip(p=0.5),
+      transforms.RandomHorizontalFlip(p=.5),
       transforms.ColorJitter(brightness=.1),
+      transforms.Grayscale(),
       transforms.ToTensor(),
       transforms.Normalize(mean=pixel_mean, std=pixel_std),
   ])
